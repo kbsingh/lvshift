bd685e32f23ef2c792a5d1c04d9c8806003eaca9
Karanbir Singh <kbsingh@karan.org>
2017-07-10 15:58:45 +0100

    need a better grep

--- scripts/find-remove.sh
+++ scripts/find-remove.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 name=$1
 
-if [ `virsh list --all --name| grep -q -r "${name}$" | wc -l` -gt 0 ]; then
+if [ `virsh list --all --name| grep ${name} | wc -l` -gt 0 ]; then
   virsh destroy ${name}
   sleep 5
   virsh undefine ${name} --remove-all-storage
