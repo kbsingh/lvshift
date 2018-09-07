602694cab4b19f0c6872b3d7f20cf73f40adaf09
Karanbir Singh <kbsingh@karan.org>
2018-09-07 18:50:06 +0100

    make it a bit faster

--- ansible/localhost.inventory.sh
+++ ansible/localhost.inventory.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-yum -y install epel-release
-yum -y install centos-release-openshift-origin
-yum -y install openshift-ansible
-# may need docker setup and docker-storage
+yum -y install wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
+yum -y install epel-release centos-release-openshift-origin
+yum -y install openshift-ansible docker origin-clients
+
+# dont want epel bits to sneak into the cluster bringup itself
+yum-config-manager --save --disablerepo=epel
+
+sed -ie "s/OPTIONS='--selinux-enabled/OPTIONS='--insecure-registry 172.30.0.0/16 --selinux-enabled/" /etc/sysconfig/docker
+systemctl start docker
+
+# may want to setup docker-storage
+
 
 ansible-playbook -i localhost.inventory /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
-ansible-playbook -i localhost.inventory /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
+if [ $? -eq 0 ]; then
+  ansible-playbook -i localhost.inventory /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
+fi
